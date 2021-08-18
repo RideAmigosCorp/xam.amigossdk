d84b2f6753b58948a5f964b489694a9d433651e1
Ben Dalton <bendalton@rideamigos.com>
2021-08-18 16:46:13 -0400

    Base functionality in place

---
+++ AmigosSDK.Abstractions/RAWebView.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace AmigosSDK.Abstractions
+{
+    public class RAWebView : FormsWebView
+    {
+        /// <summary>
+        /// A bindable property for the JWTString property.
+        /// </summary>
+        public static readonly BindableProperty JWTStringProperty = BindableProperty.Create(nameof(JWTString), typeof(String), typeof(FormsWebView), null, propertyChanged: OnJWTSTringPropertyChanged);
+
+        static void OnJWTSTringPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            Console.WriteLine("yes");
+            RAWebView.GlobalRegisteredHeaders.Add("Authorization", newValue as String);
+        }
+
+        /// <summary>
+        /// The JWT associated with the current user
+        /// </summary>
+        public String JWTString
+        {
+            get => (String)GetValue(JWTStringProperty);
+            set => SetValue(JWTStringProperty, value);
+        }
+
+        public RAWebView() {
+            this.Source = "https://linkedin.rideamigos.com/workplace-app-preview";
+            //this.Source = "https://httpbin.org/anything";
+        }
+
+    }
+}
